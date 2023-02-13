cc2c8d21c20b66ae5a04727ec17965c7c493d6c4
spleekz <Shutnik-12K@yandex.ru>
2023-02-13 17:37:51 +0300

    add deepClone fn

---
+++ processes/renderer/src/lib/deep-clone.ts
@@ -0,0 +1,11 @@
+import { isObject, objectMapAll } from './objects'
+
+export const deepClone = <T>(value: T): T => {
+  if (isObject(value)) {
+    return objectMapAll(value, (child) => deepClone(child)) as unknown as T
+  } else if (Array.isArray(value)) {
+    return value.map((child: typeof value[number]) => deepClone(child)) as unknown as T
+  } else {
+    return value
+  }
+}
