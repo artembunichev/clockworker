45ebf78ed86b346ee41582ca721586ac9cd2eccc
spleekz <Shutnik-12K@yandex.ru>
2023-02-15 18:04:52 +0300

    refactor areEquivalent

--- processes/renderer/src/lib/are-equivalent.ts
+++ processes/renderer/src/lib/are-equivalent.ts
@@ -1,17 +1,6 @@
 import { AnyObject } from 'process-shared/types/basic-utility-types'
 
-import { isObject, objectKeys } from './objects'
-
-const areEquivalentArrays = (array1: Array<any>, array2: Array<any>): boolean => {
-  const length1 = array1.length
-  const length2 = array2.length
-
-  if (length1 === length2) {
-    return array1.every((el, index) => areEquivalent(el, array2[index]))
-  }
-
-  return false
-}
+import { objectKeys } from './objects'
 
 const areEquivalentObjects = (object1: AnyObject, object2: AnyObject): boolean => {
   const length1 = objectKeys(object1).length
@@ -26,10 +15,8 @@ const areEquivalentObjects = (object1: AnyObject, object2: AnyObject): boolean =
 
 export const areEquivalent = (value1: any, value2: any): boolean => {
   if (typeof value1 === typeof value2) {
-    if (isObject(value1) && isObject(value2)) {
+    if (typeof value1 === 'object' && typeof value2 === 'object') {
       return areEquivalentObjects(value1, value2)
-    } else if (Array.isArray(value1) && Array.isArray(value2)) {
-      return areEquivalentArrays(value1, value2)
     } else if (isNaN(value1) && isNaN(value2)) {
       return true
     } else {
